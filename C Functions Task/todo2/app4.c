eed9ecaf0c30bc58e89465749593baa4d054c50f
gundaganidheeraj <78074825+gundaganidheeraj@users.noreply.github.com>
2024-02-12 22:29:53 +0530

    Update app4.c

--- C Functions Task/todo2/app4.c
+++ C Functions Task/todo2/app4.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+//called function
+void Area(int radius){
+    float pi=3.14,result;
+    result=pi*radius*radius;
+    printf("%f",result);
+}
 
+int main()
+{
+    int r=7;
+    //function caller
+    Area(r);
+    return 0;
+}
