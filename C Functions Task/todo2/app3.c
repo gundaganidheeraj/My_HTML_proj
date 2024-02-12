90d9c821f53abf112e207ff4d5d64ec0328a454d
gundaganidheeraj <78074825+gundaganidheeraj@users.noreply.github.com>
2024-02-12 22:27:08 +0530

    Update app3.c

--- C Functions Task/todo2/app3.c
+++ C Functions Task/todo2/app3.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+//called function
+void Biggest(int a,int b){
+    int result=a>b?a:b;
+    printf("%d",result);
+}
 
+int main()
+{
+    int a=10;
+    int b=20;
+    //function caller
+    Biggest(a,b);
+    return 0;
+}
