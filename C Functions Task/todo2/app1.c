9c6ded16017d8cb9c92eef6b256e7359a46606f2
gundaganidheeraj <78074825+gundaganidheeraj@users.noreply.github.com>
2024-02-12 22:17:44 +0530

    Update app1.c

--- C Functions Task/todo2/app1.c
+++ C Functions Task/todo2/app1.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+//called function
+void formula(int a,int b){
+    int result=(a*a)+(b*b)+2*(a+b);
+    print("%d",result);
+}
 
+int main()
+{
+    int a=10;
+    int b=20;
+    //function caller
+    formula(a,b);
+    return 0;
+}
