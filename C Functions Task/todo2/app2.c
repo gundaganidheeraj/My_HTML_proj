cf0e3878af47adac7b389be1d6d9fb5b0d8debc2
gundaganidheeraj <78074825+gundaganidheeraj@users.noreply.github.com>
2024-02-12 22:24:31 +0530

    Update app2.c

--- C Functions Task/todo2/app2.c
+++ C Functions Task/todo2/app2.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+//called function
+void Average(int a,int b,int c){
+    float result=(a+b+c)/3;
+    printf("%f",result);
+}
 
+int main()
+{
+    int a=10;
+    int b=20;
+    int c=30;
+    //function caller
+    Average(a,b,c);
+    return 0;
+}
