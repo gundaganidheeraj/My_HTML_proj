4fe292e664cda5b6e528934f2663f10ea16e4c67
gundaganidheeraj <78074825+gundaganidheeraj@users.noreply.github.com>
2024-02-12 22:42:14 +0530

    Update app4.java

--- C Functions Task/todo3/app4.java
+++ C Functions Task/todo3/app4.java
@@ -1,1 +1,15 @@
-
+public class Main
+{
+	public static void main(String[] args) {
+	    int r=7;
+	   	System.out.print("Area of circele of radius "+r+" is :");
+	   	//function caller
+	   	Area(r);
+	}
+	//called function
+	public static void Area(int r){
+	   double pi=3.14;
+	   double result=pi*(r*r);
+	    System.out.println(result);
+	}
+}
