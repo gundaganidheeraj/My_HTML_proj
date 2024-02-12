0d0d84cc0ea82ec3e7bf46b75e707d57870a8208
gundaganidheeraj <78074825+gundaganidheeraj@users.noreply.github.com>
2024-02-12 22:34:12 +0530

    Update app1.java

--- C Functions Task/todo3/app1.java
+++ C Functions Task/todo3/app1.java
@@ -1,1 +1,11 @@
-
+public class Main
+{
+	public static void main(String[] args) {
+	   	System.out.print("Result:");
+	   	formula(10,20);
+	}
+	public static void formula(int a,int b){
+	    int result=(a*a)+(b*b)+2*(a+b);
+	    System.out.println(result);
+	}
+}
