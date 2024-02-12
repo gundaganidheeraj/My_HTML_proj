c9fce73d59ca985babd40642e6b5539b9c372115
gundaganidheeraj <78074825+gundaganidheeraj@users.noreply.github.com>
2024-02-12 22:37:54 +0530

    Update app2.java

--- C Functions Task/todo3/app2.java
+++ C Functions Task/todo3/app2.java
@@ -2,8 +2,10 @@ public class Main
 {
 	public static void main(String[] args) {
 	   	System.out.print("Result:");
+		//function caller
 	   	Average(10,20,30);
 	}
+	//called function
 	public static void Average(int a,int b,int c){
 	    float result=(a+b+c)/3;
 	    System.out.println(result);
