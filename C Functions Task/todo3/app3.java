db763a5eb7bbb87b99efda5b0cca890e17586067
gundaganidheeraj <78074825+gundaganidheeraj@users.noreply.github.com>
2024-02-12 22:38:40 +0530

    Update app3.java

--- C Functions Task/todo3/app3.java
+++ C Functions Task/todo3/app3.java
@@ -2,8 +2,10 @@ public class Main
 {
 	public static void main(String[] args) {
 	   	System.out.print("Result:");
+		//function caller
 	   	Biggest(10,20);
 	}
+	//called function
 	public static void Biggest(int a,int b){
 	    int result=a>b?a:b;
 	    System.out.println(result);
