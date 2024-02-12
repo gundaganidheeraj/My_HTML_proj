56c116b014bc309d8e018177ad93ed63c496af22
gundaganidheeraj <78074825+gundaganidheeraj@users.noreply.github.com>
2024-02-12 22:35:10 +0530

    Update app2.java

--- C Functions Task/todo3/app2.java
+++ C Functions Task/todo3/app2.java
@@ -1,1 +1,11 @@
-
+public class Main
+{
+	public static void main(String[] args) {
+	   	System.out.print("Result:");
+	   	Biggest(10,20);
+	}
+	public static void Biggest(int a,int b){
+	    int result=a>b?a:b;
+	    System.out.println(result);
+	}
+}
